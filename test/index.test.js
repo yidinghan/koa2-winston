3d06a95880a73b46e293353e1a7d13287af4415f
ding <playdingnow@gmail.com>
2017-04-10 18:02:14 +0800

    test(keysRecorder): initial test case

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,12 @@
 const { test } = require('ava');
-const koaWinston = require('../');
+const { keysRecorder } = require('../');
 
 test('ok', (t) => {
-  t.truthy(koaWinston);
+  t.truthy(keysRecorder);
+});
+
+test('keysRecorder: should get an function as return', (t) => {
+  const recorder = keysRecorder();
+
+  t.is(typeof recorder, 'function');
 });
