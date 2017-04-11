498d465f0a5b197466cffeb640b9f1cc1ce11060
ding <playdingnow@gmail.com>
2017-04-11 09:50:51 +0800

    test(keysRecorder):one more test for defaults key

--- test/index.test.js
+++ test/index.test.js
@@ -5,8 +5,17 @@ test('ok', (t) => {
   t.truthy(keysRecorder);
 });
 
-test('keysRecorder: should get an function as return', (t) => {
+test('keysRecorder:should get an function as return', (t) => {
   const recorder = keysRecorder();
 
   t.is(typeof recorder, 'function');
+});
+
+test('keysRecorder:recorder should get defaults keys', (t) => {
+  const recorder = keysRecorder({
+    defaults: ['a'],
+  });
+  const result = recorder({ a: 1, b: 2 });
+
+  t.deepEqual(result, { a: 1 });
 });
