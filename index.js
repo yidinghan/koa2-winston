3d0a1af6a64e2a6a3319ce2660e6661c7042c024
ding <playdingnow@gmail.com>
2017-05-11 08:58:42 +0800

    docs(readme): insert jsdoc

--- index.js
+++ index.js
@@ -7,68 +7,10 @@ const onFinished = require('on-finished');
 const { format } = require('util');
 
 /**
- * kong request and respone logger example
- * {
- *     "request": {
- *         "method": "GET",
- *         "uri": "/get",
- *         "size": "75",
- *         "request_uri": "http://httpbin.org:8000/get",
- *         "querystring": {},
- *         "headers": {
- *             "host": "httpbin.org",
- *             "user-agent": "curl/7.37.1"
- *         }
- *     },
- *     "response": {
- *         "status": 200,
- *         "size": "434",
- *         "headers": {
- *             "Content-Length": "197",
- *             "via": "kong/0.3.0",
- *             "Connection": "close",
- *             "access-control-allow-credentials": "true",
- *             "Content-Type": "application/json",
- *             "server": "nginx",
- *             "access-control-allow-origin": "*"
- *         }
- *     },
- *     "authenticated_entity": {
- *         "consumer_id": "80f74eef-31b8-45d5-c525-ae532297ea8e",
- *         "id": "eaa330c0-4cff-47f5-c79e-b2e4f355207e",
- *     },
- *     "api": {
- *         "created_at": 1488830759000,
- *         "hosts": [
- *           "example.org"
- *         ],
- *         "http_if_terminated": true,
- *         "https_only": false,
- *         "id": "6378122c-a0a1-438d-a5c6-efabae9fb969",
- *         "name": "example-api",
- *         "preserve_host": false,
- *         "retries": 5,
- *         "strip_uri": true,
- *         "upstream_connect_timeout": 60000,
- *         "upstream_read_timeout": 60000,
- *         "upstream_send_timeout": 60000,
- *         "upstream_url": "http://httpbin.org"
- *     },
- *     "latencies": {
- *         "proxy": 1430,
- *         "kong": 9,
- *         "request": 1921
- *     },
- *     "started_at": 1433209822425,
- *     "client_ip": "127.0.0.1"
- * }
- */
-
-/**
  * keysRecorder
  * use ldoash pick, get and set to collect data from given target object
  *
- * @param {object} payload - input arguments
+ * @param {Object} payload - input arguments
  * @param {string[]} [payload.defaults] - default keys will be collected
  * @param {string[]} [payload.selects] - keys will be collected as
  * additional part
