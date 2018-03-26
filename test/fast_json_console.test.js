3c0b3769a87da4536afec2adee936b5dec2a3d85
ding <playdingnow@gmail.com>
2018-03-26 23:41:02 +0800

    test(fast json console): two more cases

--- test/fast_json_console.test.js
+++ test/fast_json_console.test.js
@@ -5,3 +5,25 @@ const FastJsonConsole = require('../fast_json_console');
 test('ok', (t) => {
   t.truthy(FastJsonConsole);
 });
+
+test.cb('should get log info without exception', (t) => {
+  const fastJsonConsole = new FastJsonConsole();
+
+  t.plan(2);
+  fastJsonConsole.log('info', 'test', {}, (err, flag) => {
+    t.is(err, null);
+    t.true(flag);
+    t.end();
+  });
+});
+
+test.cb('should get log error without exception', (t) => {
+  const fastJsonConsole = new FastJsonConsole();
+
+  t.plan(2);
+  fastJsonConsole.log('error', 'test', {}, (err, flag) => {
+    t.is(err, null);
+    t.true(flag);
+    t.end();
+  });
+});
