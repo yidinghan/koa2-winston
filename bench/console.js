d2fe05182e82cc305509402c26be1db71f58b48a
ding <playdingnow@gmail.com>
2018-03-27 00:21:23 +0800

    chore(bench): try process.stdout.write for better resule

--- bench/console.js
+++ bench/console.js
@@ -1,6 +1,7 @@
 /* eslint no-console: 0 */
 /* eslint import/no-extraneous-dependencies: 0 */
 const Benchmark = require('benchmark');
+const os = require('os');
 
 const stringify = require('../stringify');
 
@@ -8,14 +9,14 @@ const bench = new Benchmark('console', {
   initCount: 100,
   onCycle: event => console.log(String(event.target)),
   fn() {
-    console.log(stringify({
+    process.stdout.write(stringify({
       started_at: 1522078024245,
       duration: 388,
       level: 'info',
       message: 'HTTP get /ding',
       req: { headers: {}, url: '/ding', method: 'get' },
       res: {},
-    }));
+    }) + os.EOL);
   },
   onComplete: complete =>
     console.log({ 'total ops/sec': complete.target.hz }),
