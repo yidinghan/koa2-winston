e3559f055d13764c80237417d55d4f6fa7bfd1c4
ding <playdingnow@gmail.com>
2018-03-27 00:17:25 +0800

    chore(bench): console for comparsion

---
+++ bench/console.js
@@ -0,0 +1,24 @@
+/* eslint no-console: 0 */
+/* eslint import/no-extraneous-dependencies: 0 */
+const Benchmark = require('benchmark');
+
+const stringify = require('../stringify');
+
+const bench = new Benchmark('middleware', {
+  initCount: 100,
+  onCycle: event => console.log(String(event.target)),
+  fn() {
+    console.log(stringify({
+      started_at: 1522078024245,
+      duration: 388,
+      level: 'info',
+      message: 'HTTP get /ding',
+      req: { headers: {}, url: '/ding', method: 'get' },
+      res: {},
+    }));
+  },
+  onComplete: complete =>
+    console.log({ 'total ops/sec': complete.target.hz }),
+});
+
+bench.run();
