d12580f3ef10fc7c82a486a7e56d7cd6abd06905
ding <playdingnow@gmail.com>
2018-03-26 23:40:45 +0800

    fix(fast json console): missing default value

--- fast_json_console.js
+++ fast_json_console.js
@@ -2,10 +2,10 @@ const winston = require('winston');
 const os = require('os');
 
 class FastJsonConsoleTransport extends winston.Transport {
-  constructor({ stringify = JSON.stringify }) {
+  constructor(options = {}) {
     super();
 
-    this.stringify = stringify;
+    this.stringify = options.stringify || JSON.stringify;
   }
   log(level, message, meta, callback) {
     const output = this.stringify(Object.assign({ message, level }, meta));
