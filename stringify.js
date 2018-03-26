075151ecd24c64bdc51cb22afcae4008a6324589
ding <playdingnow@gmail.com>
2018-03-26 10:51:41 +0800

    docs(stringify): example of log object

--- stringify.js
+++ stringify.js
@@ -1,5 +1,33 @@
 const fastJson = require('fast-json-stringify');
 
+// {
+//   "req": {
+//     "headers": {
+//       "host": "127.0.0.1",
+//       "x-forwarded-for": "127.0.0.1",
+//       "x-auth": "authkey",
+//       "accept": "application/json"
+//     },
+//     "url": "/hello?foo=bar",
+//     "method": "GET",
+//     "href": "http://127.0.0.1/hello?foo=bar",
+//     "query": {
+//       "foo": "bar"
+//     }
+//   },
+//   "started_at": 1522032522164,
+//   "res": {
+//     "headers": {
+//       "content-type": "application/json; charset=utf-8",
+//       "content-length": "281"
+//     },
+//     "status": 200
+//   },
+//   "duration": 2,
+//   "level": "info",
+//   "message": "HTTP GET /hello?foo=bar"
+// }
+
 const schema = {
   title: 'koa2 logger',
   type: 'object',
