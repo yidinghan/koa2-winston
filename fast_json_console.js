897ba467a986b9fe8246617ecfac144e3fc15642
ding <playdingnow@gmail.com>
2018-04-03 16:18:53 +0800

    feat(stringify): add flatstr method

--- fast_json_console.js
+++ fast_json_console.js
@@ -1,21 +1,32 @@
 const winston = require('winston');
 const os = require('os');
 
+const flatstr = (s) => {
+  Number(s);
+  return s;
+};
+
+const stdwrite = (level, log) => {
+  if (level === 'error') {
+    process.stderr.write(log + os.EOL);
+  } else {
+    process.stdout.write(log + os.EOL);
+  }
+};
+
+const flatwrite = (level, log) => stdwrite(level, flatstr(log));
+
 class FastJsonConsoleTransport extends winston.Transport {
   constructor(options = {}) {
     super();
 
     this.stringify = options.stringify || JSON.stringify;
+    this.flatstr = options.flatstr || false;
+    this.write = this.flatstr ? flatwrite : stdwrite;
   }
   log(level, message, meta, callback) {
-    const output = this.stringify(Object.assign({ message, level }, meta));
-    if (level === 'error') {
-      process.stderr.write(output + os.EOL);
-    } else {
-      process.stdout.write(output + os.EOL);
-    }
+    this.write(this.stringify(Object.assign({ message, level }, meta)));
 
-    this.emit('logged');
     callback(null, true);
   }
 }
