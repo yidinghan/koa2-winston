a678b15617e296d075109df3af6acff47b3f6d4d
ding <playdingnow@gmail.com>
2018-07-29 20:26:01 +0800

    chore(bench): use argv to control suite case
    Signed-off-by: ding <playdingnow@gmail.com>

--- bench/schema-copy.js
+++ bench/schema-copy.js
@@ -37,8 +37,8 @@ const simpleCopy = obj => JSON.parse(JSON.stringify(obj));
 const TEST_OBJ = {
   a: 'ding',
   b: {
-    c: '3',
-    d: 3,
+    c: 3,
+    d: '3',
     e: {
       f: true,
       g: Array(100)
@@ -48,6 +48,8 @@ const TEST_OBJ = {
   },
 };
 
+const argv = process.argv.slice(2).join('');
+
 const getOptions = name => ({
   // initCount: 100,
   // onCycle: ({ target }) => console.log(String(target)),
@@ -55,10 +57,22 @@ const getOptions = name => ({
     console.log('total ops/sec', { [name]: parseInt(hz, 10) }),
 });
 if (!module.parent) {
-  suite
-    .add('schemaCopy', () => schemaCopy(TEST_OBJ), getOptions('schemaCopy'))
-    .add('simpleCopy', () => simpleCopy(TEST_OBJ), getOptions('simpleCopy'))
-    .run();
+  if (/schema/.test(argv)) {
+    suite.add(
+      'schemaCopy',
+      () => schemaCopy(TEST_OBJ),
+      getOptions('schemaCopy'),
+    );
+  }
+  if (/simple/.test(argv)) {
+    suite.add(
+      'simpleCopy',
+      () => simpleCopy(TEST_OBJ),
+      getOptions('simpleCopy'),
+    );
+  }
+
+  suite.run();
 }
 
 module.exports = { simpleCopy, schemaCopy };
