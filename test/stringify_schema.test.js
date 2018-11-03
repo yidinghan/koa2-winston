2f7c0b4a75fe4d73eafc69e74a02b26e88191d90
ding <playdingnow@gmail.com>
2018-11-03 16:47:20 +0800

    test(stringify): case on empty input
    Signed-off-by: ding <playdingnow@gmail.com>

--- test/stringify_schema.test.js
+++ test/stringify_schema.test.js
@@ -2,5 +2,40 @@ const { test } = require('ava');
 const { generateSchema } = require('../stringify_schema');
 
 test('should get default schema on definitions', (t) => {
-
+  const schema = generateSchema({});
+  t.deepEqual(schema, {
+    title: 'koa2-winston-info',
+    definitions: {
+      req: {
+        properties: {
+          headers: {
+            type: 'object',
+            additionalProperties: { type: 'string' },
+            properties: { cookie: { type: 'null' } },
+          },
+          url: { type: 'string' },
+          method: { type: 'string' },
+          httpVersion: {},
+          href: { type: 'string' },
+          query: { type: 'object', additionalProperties: { type: 'string' } },
+          length: { type: 'integer' },
+        },
+      },
+      res: {
+        properties: {
+          headers: { type: 'object', additionalProperties: { type: 'string' } },
+          status: { type: 'string' },
+        },
+      },
+    },
+    type: 'object',
+    properties: {
+      started_at: { type: 'integer' },
+      duration: { type: 'integer' },
+      level: { type: 'string' },
+      message: { type: 'string' },
+      req: { $ref: '#/definitions/req' },
+      res: { $ref: '#/definitions/res' },
+    },
+  });
 });
