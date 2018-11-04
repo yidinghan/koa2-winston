3a8bdec501169c919cc0973b172dae6baaa8b7b1
ding <playdingnow@gmail.com>
2018-11-04 22:26:02 +0800

    refactor(stringify): schemaKeysHandlers fn
    Signed-off-by: ding <playdingnow@gmail.com>

--- stringify_schema.js
+++ stringify_schema.js
@@ -1,50 +1,51 @@
 const set = require('lodash.set');
 const get = require('lodash.get');
 
-const defaultReqSchema = {
-  title: 'koa2-winston-info-req',
-  type: 'object',
-  properties: {
-    headers: {
-      type: 'object',
-      additionalProperties: { type: 'string' },
+const defaultSchemas = {
+  res: {
+    title: 'koa2-winston-info-res',
+    type: 'object',
+    properties: {
+      headers: {
+        type: 'object',
+        additionalProperties: { type: 'string' },
+      },
+      status: { type: 'string' },
+      body: {
+        type: 'object',
+        additionalProperties: true,
+      },
     },
-    url: { type: 'string' },
-    method: { type: 'string' },
-    href: { type: 'string' },
-    query: {
-      type: 'object',
-      additionalProperties: { type: 'string' },
-    },
-    origin: { type: 'string' },
-    originalUrl: { type: 'string' },
-    path: { type: 'string' },
-    querystring: { type: 'string' },
-    search: { type: 'string' },
-    hostname: { type: 'string' },
-    URL: { type: 'string' },
-    type: { type: 'string' },
-    charset: { type: 'string' },
-    protocol: { type: 'string' },
-    secure: { type: 'string' },
-    ip: { type: 'string' },
-    httpVersion: { type: 'string' },
-    length: { type: 'integer' },
   },
-};
-
-const defaultResSchema = {
-  title: 'koa2-winston-info-res',
-  type: 'object',
-  properties: {
-    headers: {
-      type: 'object',
-      additionalProperties: { type: 'string' },
-    },
-    status: { type: 'string' },
-    body: {
-      type: 'object',
-      additionalProperties: true,
+  req: {
+    title: 'koa2-winston-info-req',
+    type: 'object',
+    properties: {
+      headers: {
+        type: 'object',
+        additionalProperties: { type: 'string' },
+      },
+      url: { type: 'string' },
+      method: { type: 'string' },
+      href: { type: 'string' },
+      query: {
+        type: 'object',
+        additionalProperties: { type: 'string' },
+      },
+      origin: { type: 'string' },
+      originalUrl: { type: 'string' },
+      path: { type: 'string' },
+      querystring: { type: 'string' },
+      search: { type: 'string' },
+      hostname: { type: 'string' },
+      URL: { type: 'string' },
+      type: { type: 'string' },
+      charset: { type: 'string' },
+      protocol: { type: 'string' },
+      secure: { type: 'string' },
+      ip: { type: 'string' },
+      httpVersion: { type: 'string' },
+      length: { type: 'integer' },
     },
   },
 };
@@ -66,11 +67,32 @@ const defaultInfoSchema = {
   },
 };
 
-const DOT_RE = /\./;
+const DOT_RE = /\./g;
 /**
  * @param {string} path
  */
 const asJsonSchemaPath = path => path.replace(DOT_RE, '.properties.');
+
+/**
+ * @param {string[]} keys - schema keys
+ */
+const schemaKeysHandler = (keys, handler) => keys
+  .map(asJsonSchemaPath)
+  .map(path => `properties.${path}`)
+  .map(handler);
+
+const schemaKeysHandlers = ({
+  keys, select, unselect, schema,
+}) => {
+  const reqSchema = {};
+  schemaKeysHandler(keys.concat(select), (path) => {
+    set(reqSchema, path, get(schema, path, {}));
+  });
+  schemaKeysHandler(unselect, (path) => {
+    set(reqSchema, path, { type: 'null' });
+  });
+  return reqSchema;
+};
 
 /**
  * logger middleware for koa2 use winston
@@ -84,56 +106,36 @@ const asJsonSchemaPath = path => path.replace(DOT_RE, '.properties.');
  * @param {string[]} [payload.resUnselect=[]] - response field will be removed from the log
  */
 const generateSchema = (payload) => {
-  const {
-    reqUnselect = ['headers.cookie'],
-    reqSelect = [],
-    reqKeys = [
-      'headers',
-      'url',
-      'method',
-      'httpVersion',
-      'href',
-      'query',
-      'length',
-    ],
-    resUnselect = [],
-    resSelect = [],
-    resKeys = ['headers', 'status'],
-  } = payload;
+  const options = Object.assign(
+    {
+      reqUnselect: ['headers.cookie'],
+      reqSelect: [],
+      reqKeys: [
+        'headers',
+        'url',
+        'method',
+        'httpVersion',
+        'href',
+        'query',
+        'length',
+      ],
+      resUnselect: [],
+      resSelect: [],
+      resKeys: ['headers', 'status'],
+    },
+    payload,
+  );
 
-  const reqSchema = {};
-  reqKeys
-    .concat(reqSelect)
-    .map(asJsonSchemaPath)
-    .map(path => `properties.${path}`)
-    .forEach((path) => {
-      set(reqSchema, path, get(defaultReqSchema, path, {}));
+  ['req', 'res'].forEach((prefix) => {
+    defaultInfoSchema.definitions[prefix] = schemaKeysHandlers({
+      keys: options[`${prefix}Keys`],
+      select: options[`${prefix}Select`],
+      unselect: options[`${prefix}Unselect`],
+      schema: defaultSchemas[prefix],
     });
-  reqUnselect
-    .map(asJsonSchemaPath)
-    .map(path => `properties.${path}`)
-    .forEach((path) => {
-      set(reqSchema, path, { type: 'null' });
-    });
-  defaultInfoSchema.definitions.req = reqSchema;
-
-  const resSchema = {};
-  resKeys
-    .concat(resSelect)
-    .map(asJsonSchemaPath)
-    .map(path => `properties.${path}`)
-    .forEach((path) => {
-      set(resSchema, path, get(defaultResSchema, path, {}));
-    });
-  resUnselect
-    .map(asJsonSchemaPath)
-    .map(path => `properties.${path}`)
-    .forEach((path) => {
-      set(resSchema, path, { type: 'null' });
-    });
-  defaultInfoSchema.definitions.res = resSchema;
+  });
 
   return defaultInfoSchema;
 };
 
-module.exports = { defaultInfoSchema, generateSchema };
+module.exports = { defaultInfoSchema, generateSchema, asJsonSchemaPath };
